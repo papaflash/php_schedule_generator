4a6453e5768d9a0df2037949ac65a495ce89ebd7
Alex Shagov <shagov25@gmail.com>
2024-12-20 01:57:42 +0300

    завершил реализацию по основному заданию

--- index.php
+++ index.php
@@ -3,37 +3,85 @@ declare(strict_types=1);
 
 setlocale(LC_TIME, 'ru_RU.UTF-8');
 
-showDays();
+generateShedule('12', '24');
 
-function showDays(): void
+
+function generateShedule(string $month, string $year, int $countMonths = 1): void
 {
-    $currentDate = new DateTime();
-    $firstDay = new DateTime($currentDate->format('Y-m-01'));
-    $lastDay = new DateTime($currentDate->format('Y-m-t'));
+    try{
+        $dateString = "$year-$month-01";
+        $sourceDate = new DateTime($dateString);
+        calculateShedule($sourceDate);
+    }catch (DateMalformedStringException $ex){
+        echo 'Не верно указана дата для расчета!' . PHP_EOL;
+        echo 'Расчет будет сделан по текущей дате!' . PHP_EOL;
+        $sourceDate = new DateTime();
+        calculateShedule($sourceDate);
+    }
+}
+
+function calculateShedule(DateTime $sourceDate): void
+{
+    define("WORKED_DAY_STEP", 3);
+    $firstDay = new DateTime($sourceDate->format('Y-m-01'));
+    $lastDay = new DateTime($sourceDate->format('Y-m-t'));
     $daysInMonth = [];
 
     $monthFormatter = new IntlDateFormatter(
-      'ru-RU',
-      IntlDateFormatter::SHORT,
-      IntlDateFormatter::NONE,
-      'Europe/Moscow',
-      IntlDateFormatter::GREGORIAN,
-      'MMM'
+        'ru-RU',
+        IntlDateFormatter::SHORT,
+        IntlDateFormatter::NONE,
+        'Europe/Moscow',
+        IntlDateFormatter::GREGORIAN,
+        'MMM y'
     );
     $dayFormatter = new IntlDateFormatter(
-       'ru-RU',
-       IntlDateFormatter::SHORT,
-       IntlDateFormatter::NONE,
-       'Europe/Moscow',
-       IntlDateFormatter::GREGORIAN,
-       'd eeee'
+        'ru-RU',
+        IntlDateFormatter::SHORT,
+        IntlDateFormatter::NONE,
+        'Europe/Moscow',
+        IntlDateFormatter::GREGORIAN,
+        'd eeee'
     );
+    $workedIndex = 1;
     While($firstDay <= $lastDay){
-        $daysInMonth[] = $dayFormatter->format($firstDay);
+        $dayName = $dayFormatter->format($firstDay);
+        if(isWeekend($dayName) && count($daysInMonth) !== 0) {
+            $daysInMonth[] = "\033[31m" . $dayName . "\033[0m";
+            $workedIndex = 3;
+        }
+        else if($workedIndex === WORKED_DAY_STEP){
+            $daysInMonth[] = "\033[32m" . $dayName . "\033[0m";
+            $workedIndex = 1;
+        }
+        else if(count($daysInMonth) === 0){
+            $daysInMonth[] = "\033[32m" . $dayName . "\033[0m";
+            $workedIndex++;
+        }else{
+            $daysInMonth[] = "\033[31m" . $dayName . "\033[0m";
+            $workedIndex++;
+        }
         $firstDay->modify('+1 day');
+
     }
-    echo 'Текущий месяц: ' . $monthFormatter->format(time()). PHP_EOL;
+    echo 'Расчет для: ' . $monthFormatter->format($sourceDate). PHP_EOL;
+    showShedule($daysInMonth);
+
+}
+function isWeekend(string $dayName): bool
+{
+    $workingDays = ['суббота', 'воскресенье'];
+    foreach ($workingDays as $day) {
+        if(stripos($dayName, $day) !== false){
+            return true;
+        }
+    }
+    return false;
+
+}
+function showShedule(array $daysInMonth): void
+{
     foreach ($daysInMonth as $day){
-        echo $day . PHP_EOL;
+        echo "\t" . $day . PHP_EOL;
     }
 }
