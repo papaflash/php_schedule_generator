f9d4565977701f2634a543947fb8c99bb09843a2
alexandr shagov <shagov25@gmail.com>
2024-12-20 15:44:28 +0300

    Переделал логику выбора рабочих дней, исправил ошибки при формирование массива дней, изменил вывод календаря

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 setlocale(LC_TIME, 'ru_RU.UTF-8');
 
-generateShedule('12', '24');
+generateShedule('10', '24');
 
 
 function generateShedule(string $month, string $year, int $countMonths = 1): void
@@ -22,7 +22,7 @@ function generateShedule(string $month, string $year, int $countMonths = 1): voi
 
 function calculateShedule(DateTime $sourceDate): void
 {
-    define("WORKED_DAY_STEP", 3);
+
     $firstDay = new DateTime($sourceDate->format('Y-m-01'));
     $lastDay = new DateTime($sourceDate->format('Y-m-t'));
     $daysInMonth = [];
@@ -41,47 +41,51 @@ function calculateShedule(DateTime $sourceDate): void
         IntlDateFormatter::NONE,
         'Europe/Moscow',
         IntlDateFormatter::GREGORIAN,
-        'd eeee'
+        'd eee'
     );
-    $workedIndex = 1;
     While($firstDay <= $lastDay){
-        $dayName = $dayFormatter->format($firstDay);
-        if(isWeekend($dayName) && count($daysInMonth) !== 0) {
-            $daysInMonth[] = "\033[31m" . $dayName . "\033[0m";
-            $workedIndex = 3;
-        }
-        else if($workedIndex === WORKED_DAY_STEP){
-            $daysInMonth[] = "\033[32m" . $dayName . "\033[0m";
-            $workedIndex = 1;
-        }
-        else if(count($daysInMonth) === 0){
-            $daysInMonth[] = "\033[32m" . $dayName . "\033[0m";
-            $workedIndex++;
-        }else{
-            $daysInMonth[] = "\033[31m" . $dayName . "\033[0m";
-            $workedIndex++;
-        }
+        $daysInMonth[] = "\033[31m" . $dayFormatter->format($firstDay) . "\033[0m";
         $firstDay->modify('+1 day');
-
     }
-    echo 'Расчет для: ' . $monthFormatter->format($sourceDate). PHP_EOL;
+    echo 'Расчет для: ' . "\033[32m" . $monthFormatter->format($sourceDate) . "\033[0m" . PHP_EOL;
+    setWorkingDays($daysInMonth);
     showShedule($daysInMonth);
-
+}
+function setWorkingDays(array &$days): void
+{
+    define("WORKED_DAY_STEP", 3);
+    for($i = 0, $countDays = count($days); $i < $countDays; $i += WORKED_DAY_STEP){
+        if(!isWeekend($days[$i])){
+            $days[$i] = preg_replace('/\033\[[0-9;]*m/', '', $days[$i]);
+        }else if($i === 0){
+            $days[$i] = preg_replace('/\033\[[0-9;]*m/', '', $days[$i]);
+        }else {
+            while (isWeekend($days[$i])){
+                $i++;
+            }
+            $days[$i] = preg_replace('/\033\[[0-9;]*m/', '', $days[$i]);
+        }
+    }
 }
 function isWeekend(string $dayName): bool
 {
-    $workingDays = ['суббота', 'воскресенье'];
+    $workingDays = ['ст', 'вс'];
     foreach ($workingDays as $day) {
         if(stripos($dayName, $day) !== false){
             return true;
         }
     }
     return false;
-
 }
 function showShedule(array $daysInMonth): void
 {
-    foreach ($daysInMonth as $day){
-        echo "\t" . $day . PHP_EOL;
+    $j = 0;
+    foreach ($daysInMonth as $day) {
+        if($j === 6){
+            $j = 0;
+            echo PHP_EOL;
+        }
+        echo "\t" . $day;
+        $j++;
     }
 }
